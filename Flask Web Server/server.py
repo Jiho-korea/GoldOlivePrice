439637e10e8896e8d29741afb7f9708314bfc286
Jiho-korea <ghok1027@naver.com>
2019-08-11 21:23:14 +0900

    플라스크웹서버와 텐서플로우를연동하였습니다.
    웹서버에서 최저시급을 입력하면 서버로 넘어가 텐서플로우가 치킨가격을 예측해 줍니다.

--- Flask Web Server/server.py
+++ Flask Web Server/server.py
@@ -1,12 +1,58 @@
 ﻿# -*- coding: utf-8 -*-
 from flask import Flask, render_template, request
 
+import datetime
+import tensorflow as tf
+import numpy as np
+
 app = Flask(__name__)
 
+# 플레이스 홀더를 설정합니다.
+X = tf.placeholder(tf.float32, shape=[None,1])
+Y = tf.placeholder(tf.float32, shape=[None,1])
 
-@app.route("/", methods=['GET','POST'])
+W = tf.Variable(tf.random_normal([1,1]), name="weight")
+b = tf.Variable(tf.random_normal([1]), name="bias")
+# 가설을 설정합니다.
+
+hypothesis = tf.matmul(X, W) + b
+
+# 저장된 모델을 불러오는 객체를 선언합니다.
+saver = tf.train.Saver()
+model = tf.global_variables_initializer()
+
+# 세션 객체를 생성합니다.
+sess = tf.Session()
+sess.run(model)
+
+# 저장된 모델을 세션에 적용합니다.
+save_path = "./model/saved.cpkt"
+saver.restore(sess, save_path)
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
-    
+    if request.method == 'GET':
+        return render_template('index.html')
+    if request.method == 'POST':
+        # 파라미터를 전달 받습니다.
+        mhw = float(request.form['mhw'])
+        
+
+        # 최저 시급 변수를 선언합니다.
+        price = 0
+
+        # 입력된 파라미터를 배열 형태로 준비합니다.
+        data = ((mhw), (0))
+        arr = np.array(data, dtype=np.float32)
+
+        # 입력 값을 토대로 예측 값을 찾아냅니다.
+        x_data = arr[0:1]
+        dict = sess.run(hypothesis, feed_dict={X: [x_data]})
+            
+        # 결과 배추 가격을 저장합니다.
+        price = dict[0]
+
+        return render_template('index.html', price=price)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+   app.run(debug = True)
